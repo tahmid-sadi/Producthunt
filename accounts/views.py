14efafe6bb49677c038190f99fd0679c223d4078
tahmid-sadi <tahbaquee@gmail.com>
2019-10-16 22:09:22 +0600

    Commiting, after editing.

--- accounts/views.py
+++ accounts/views.py
@@ -23,12 +23,12 @@ def signup(request):
 
 def login(request):
     if request.method == 'POST':
-        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
         if user is not None:
             auth.login(request, user)
             return redirect('home')
         else:
-            return render(request, 'accounts/login.html', {'error':'Username or Password is incorrect.'})
+            return render(request, 'accounts/login.html', {'error': 'Username or Password is incorrect.'})
     else:
         return render(request, 'accounts/login.html')
 
