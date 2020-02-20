459e0ee1fddbed16108c4d5574bffac64b501f71
tahmid-sadi <tahbaquee@gmail.com>
2020-02-20 08:52:16 +0600

    After a long time. Just commiting to get things up to date.

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib import auth
 
 def signup(request):
     if request.method == 'POST':
-        #User has info and wants an account now
+        # User has info and wants an account now. sadi:rumki
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.get(username=request.POST['username'])
